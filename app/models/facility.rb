2cc24e054273f918d0a41b9236c55dedd365589f
Ryan Kretch <rkretch91@gmail.com>
2017-12-27 00:10:30 +0800

    fixed array problem

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,7 +1,8 @@
 class Facility < ApplicationRecord
+  # TYPES = ["Restroom", "Nursing Room", "Family Room", "Makeup Room", "Accessible Room"]
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
-  validates :types, inclusion: { in: ["Restroom", "Nursing Room", "Family Room", "Makeup Room", "Accessible Room"]}
+  validates :types, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
