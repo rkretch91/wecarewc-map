b5554d1a6b34c7dff051808e39bdc1a2223590f3
Ryan Kretch <rkretch91@gmail.com>
2018-01-04 16:47:04 +0800

    baidu maps api now works :)

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,13 +1,13 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  lookup: :bing,            # name of geocoding service (symbol)
+  lookup: :baidu,            # name of geocoding service (symbol)
   # ip_lookup: :freegeoip,      # name of IP address geocoding service (symbol)
   # language: :en,               # ISO-639 language code
-  use_https: true,           # use HTTPS for lookup requests? (if supported)
+  # use_https: true,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: "NPMyDgVWqvLs2AOFLueU~oLiYaBtvX9SLaK1IbuzLVQ~Am-d4hWIePX5Yp1abJufTuic-cehGeSkdHAsb7nYhmuQQpQS18zf6g789mxLaW7t",              # API key for geocoding service
+  api_key: "3UKSHeiMEMa3dnGG2e3EMTC5WYXRa1TM",              # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
